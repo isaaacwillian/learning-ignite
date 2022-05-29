24940dc333f2bb79531c1601b56f48891fa4bf82
Isaac Willian <isaaacwillian@gmail.com>
2022-05-29 16:13:40 -0300

    Added Middleware: get customer via header

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf == cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
@@ -28,10 +42,8 @@ app.post("/account", (req, res) => {
   res.status(201).send("ok");
 });
 
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = req.params;
-
-  const customer = customers.find((customer) => customer.cpf == cpf);
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
 
   return res.json(customer.statement);
 });
